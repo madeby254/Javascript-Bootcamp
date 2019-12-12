602c7ef0ce020b857e0a0c00247f1b092cb4ec20
Mohamed Nuh <54922319+madeby254@users.noreply.github.com>
2019-12-11 19:45:54 -0800

    Advanced boolean

---
+++ boolean-advanced.js
@@ -0,0 +1,11 @@
+let isAccountLocked = false
+let userRole = 'user'
+
+
+if (isAccountLocked) {
+    console.log('Is account locked')
+} else if (userRole === 'admin') {
+    console.log('Hey Admin')
+} else {
+    console.log('Welcome')
+}
