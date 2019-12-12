eca7d438649d0d0821411c1a7451ea09f2919da8
Mohamed Nuh <54922319+madeby254@users.noreply.github.com>
2019-12-12 15:39:52 -0800

    Challenge Logical

---
+++ challenge.js
@@ -0,0 +1,13 @@
+let isGuestOneVegan = false
+let isGuestTwoVegan = false
+
+if (isGuestOneVegan && isGuestTwoVegan) {
+    console.log('Welcome to veganism')
+} else if (isGuestOneVegan || isGuestTwoVegan) {
+    console.log("He is a mix menu")
+}
+else {
+    console.log("enjoy our full menu")
+}
+
+
