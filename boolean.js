a695ef0208fbef9b258645e620696d17647e8fbc
Mohamed Nuh <54922319+madeby254@users.noreply.github.com>
2019-12-11 19:47:58 -0800

    replaced

--- boolean.js
+++ boolean.js
@@ -5,23 +5,26 @@
 // <= - less than or equal to operator
 // >= - greater than or equal to opeartor
 
-// let temp = 12
+let temp = 180
 
-// if (temp <= 32) {
-//     console.log('its Fucking Freezing!')
-// }
-
-// if (temp >= 80) {
-//     console.log('Its hell outside stay inside')
-// }
-
-
-let age = 29
-
-if (age >= 65) {
-    console.log('Hello you qualify for a Senior Discount')
+if (temp <= 32) {
+    console.log('It is freezing outside!')
 }
 
+if (temp >= 110) {
+    console.log('It is way to hot outside!')
+}
+
+// Challenge area
+
+let age = 6
+
+// If 7 or under print message about child pricing
 if (age <= 7) {
-    console.log('Hello that peasant of a child has a discount')
+    console.log('You will get a child discount!')
+}
+
+// If 65 or older print message about senior discount
+if (age >= 65) {
+    console.log('You will get a senior discount!')
 }
