f9615dd33ae28e5d7bd1646881bfa46d55e9351b
Mohamed Nuh <54922319+madeby254@users.noreply.github.com>
2019-12-11 17:45:01 -0800

    Boolean

---
+++ booleans.js
@@ -0,0 +1,12 @@
+// let temp = 31
+// let isFreezing = temp === 31
+
+// console.log(isFreezing)
+
+
+let age = 70
+let isChild = age <= 7
+let isage = age >= 65
+
+console.log(isChild)
+console.log(isage)
