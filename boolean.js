483780d83bcb6bab89e2156360ad7b5bc07ea47c
Mohamed Nuh <54922319+madeby254@users.noreply.github.com>
2019-12-11 21:32:42 -0800

    Bollead

--- boolean.js
+++ boolean.js
@@ -5,26 +5,19 @@
 // <= - less than or equal to operator
 // >= - greater than or equal to opeartor
 
-let temp = 180
+let temp = 11
 
 if (temp <= 32) {
     console.log('It is freezing outside!')
+} else if (temp >= 85) {
+    console.log('It is hot outside')
+} else {
+    console.log('Go for it. It is pretty nice')
 }
 
-if (temp >= 110) {
-    console.log('It is way to hot outside!')
-}
 
-// Challenge area
 
-let age = 6
 
-// If 7 or under print message about child pricing
-if (age <= 7) {
-    console.log('You will get a child discount!')
-}
-
-// If 65 or older print message about senior discount
-if (age >= 65) {
-    console.log('You will get a senior discount!')
-}
+// It is Freezing outside!
+// It is hot outside!
+// Go for it. It is pretty nice.
